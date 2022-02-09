a573f18b1665aadc84a62affaff72422e4ce50b4
Hugo Jose <hugojose39@yahoo.com>
2022-02-09 20:35:47 -0300

    creating static pages

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,15 @@ use App\Http\Controllers\Site\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('site')->group(function () {
-    Route::get('/', HomeController::class);
+    Route::get('/', HomeController::class)->name('site.home');
 
-    Route::get('/products', [CategoryController::class, 'index']);
-    Route::get('/products/{slug}', [CategoryController::class, 'show']);
+    Route::get('/products', [CategoryController::class, 'index'])->name('site.products');
+    Route::get('/products/{slug}', [CategoryController::class, 'show'])->name('site.products.category');
 
-    Route::get('/blog', BlogController::class);
+    Route::get('/blog', BlogController::class)->name('site.blog');
 
-    Route::view('sobre', 'site.about.index');
+    Route::view('/about', 'site.about.index')->name('site.about');
 
-    Route::get('/contact', [ContactController::class, 'index']);
-    Route::post('/contact', [ContactController::class, 'form']);
+    Route::get('/contact', [ContactController::class, 'index'])->name('site.contact');
+    Route::post('/contact', [ContactController::class, 'form'])->name('site.contact.form');
 });
